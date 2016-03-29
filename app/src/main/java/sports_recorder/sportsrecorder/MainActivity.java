0d79760e325d9da9c9bc3586cbd9c70df583f386
Antoine <adejong@illinois.edu>
2016-03-29 15:34:53 -0500

    Workaround for bug causing emulator to crash when changing orientation.

--- app/src/main/java/sports_recorder/sportsrecorder/MainActivity.java
+++ app/src/main/java/sports_recorder/sportsrecorder/MainActivity.java
@@ -1,6 +1,9 @@
 package sports_recorder.sportsrecorder;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -14,6 +17,20 @@ public class MainActivity extends Activity {
         ImageView iv = (ImageView)findViewById(R.id.imageView);
         iv.setImageResource(R.drawable.football_pitch);
         iv.setScaleType(ImageView.ScaleType.FIT_XY);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Fix from stackoverflow for crashing on rotation.
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        Drawable drawable = imageView.getDrawable();
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            Bitmap bitmap = bitmapDrawable.getBitmap();
+            bitmap.recycle();
+        }
+
+        super.onDestroy();
     }
 
 
