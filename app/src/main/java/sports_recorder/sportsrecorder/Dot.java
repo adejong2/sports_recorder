156d78b2f562366a4f02d5fe956a1f84df63f000
Antoine <adejong@illinois.edu>
2016-04-01 20:47:46 -0500

    Button images

--- app/src/main/java/sports_recorder/sportsrecorder/Dot.java
+++ app/src/main/java/sports_recorder/sportsrecorder/Dot.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 /**
  * Created by Antoine on 4/1/2016.
  */
+// Dot class
 public class Dot implements Serializable {
     public float x, y;
     public int timestamp;
