9173650686ca277df22233137ce0b56424471fd4
Antoine <adejong@illinois.edu>
2016-04-05 23:44:32 -0500

    expanded summary screen

--- app/src/main/java/sports_recorder/sportsrecorder/SummaryDetailFragment.java
+++ app/src/main/java/sports_recorder/sportsrecorder/SummaryDetailFragment.java
@@ -1,16 +1,16 @@
-package sports_recorder.sportsrecorder;
+        package sports_recorder.sportsrecorder;
 
 
-import android.app.Activity;
-import android.app.Fragment;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
+        import android.app.Activity;
+        import android.app.Fragment;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
 public class SummaryDetailFragment extends Fragment {
     private GameManager gm = GameManager.getInstance();
@@ -18,20 +18,46 @@ public class SummaryDetailFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-     return inflater.inflate(R.layout.fragment_summary_detail, container, false);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_summary_detail, container, false);
     }
 
     // Tell the fragment which game's stats to display
     public void loadPosition(int position) {
         Game g = gm.get(position);
         game = g;
+
         Activity activity = getActivity();
         TextView tv = (TextView) activity.findViewById(R.id.editDateText);
         tv.setText(g.gameDateStr);
 
         tv = (TextView) activity.findViewById(R.id.editScoreText);
-        tv.setText(g.scoreA + " to " + g.scoreB);
+        String s = g.scoreA + " to " + g.scoreB;
+        tv.setText(s);
+
+        tv = (TextView) activity.findViewById(R.id.editHalfScoreText);
+        s = g.halfScoreA + " to " + g.halfScoreB;
+        tv.setText(s);
+
+        tv = (TextView) activity.findViewById(R.id.editTimeText);
+        s = "" + g.timeOnClock;
+        tv.setText(s);
+
+        tv = (TextView) activity.findViewById(R.id.editGoalText);
+        s = "" + g.goals;
+        tv.setText(s);
+
+        tv = (TextView) activity.findViewById(R.id.editSOGText);
+        s = "" + g.shotsOnGoal;
+        tv.setText(s);
+
+        tv = (TextView) activity.findViewById(R.id.editShotsText);
+        s = "" + g.shots;
+        tv.setText(s);
+
+        tv = (TextView) activity.findViewById(R.id.editPenaltiesText);
+        s = "" + g.penalties;
+        tv.setText(s);
     }
 
     public void onClick(View view) {
